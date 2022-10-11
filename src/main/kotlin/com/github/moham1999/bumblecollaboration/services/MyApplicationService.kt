d1244446ba0219d8f3448f24012a0247f864b27e
GitHub Action <action@github.com>
2022-10-11 15:31:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/moham1999/bumblecollaboration/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/moham1999/bumblecollaboration/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.moham1999.bumblecollaboration.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.moham1999.bumblecollaboration.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/moham1999/bumblecollaboration/services/MyProjectService.kt
+++ src/main/kotlin/com/github/moham1999/bumblecollaboration/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.moham1999.bumblecollaboration.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.moham1999.bumblecollaboration.MyBundle
 
 class MyProjectService(project: Project) {
 
